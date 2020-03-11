f18f72c31c9746420f201ca8747de13994830444
Amber-Pittman <amber@amberpittman.com>
2020-03-11 19:41:44 -0400

    Created dir and file. Moved fetchShow to it.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
+import {fetchShow} from "./api/fetchShow";
+
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
@@ -15,17 +17,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    fetchShow() 
         .then(res => {
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
-    fetchShow();
   }, []);
 
   const handleSelect = e => {
@@ -38,7 +34,9 @@ export default function App() {
 
   return (
     <div className="App">
-      <img className="poster-img" src={show.image.original} alt={show.name} />
+      <img className="poster-img" 
+        src={show.image.original} 
+        alt={show.name} />
       <h1>{show.name}</h1>
       {parse(show.summary)}
       <Dropdown
---
+++ src/api/fetchShow.js
@@ -0,0 +1,12 @@
+import Axios from "axios";
+
+export const fetchShow = () => {
+    return Axios
+    .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(res => {
+          console.log(res);
+          return res
+      });
+}
